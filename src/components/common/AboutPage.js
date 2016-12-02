7bff810c7de39f2dcaafb85aeee3384dfdb9fd32
Justin Joyce <jjoyce07@yahoo.com>
2016-12-01 21:49:59 -0600

    added about page

---
+++ src/components/common/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+
+        <p>Lorem ipsum dolor si amet, sonsecteutu elit.</p>
+    </div>
+  );
+}
+
+export default About;
